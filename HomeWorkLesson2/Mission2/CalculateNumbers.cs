a18dc799850afc5e4c696f10643a879beaf562a7
enemy <nkalmykov937@gmail.com>
2019-01-29 20:24:51 +0300

    Выполнил задание номер 2: Написать метод подсчета количества цифр числа.

---
+++ HomeWorkLesson2/Mission2/CalculateNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    class CalculateNumbers
+    {
+        public static int Calculate(string s)
+        {
+            return s.Length;
+        }
+    }
+}
---
+++ HomeWorkLesson2/Mission2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Калмыков
+//2. Написать метод подсчета количества цифр числа.
+
+namespace Mission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Число: ");
+            Console.WriteLine("Цифр: " + CalculateNumbers.Calculate(Console.ReadLine()));
+            Console.ReadKey();
+
+        }
+
+        
+    }
+}
