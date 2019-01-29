095636f6500d89b210c2f7c12834078949543a51
enemy <nkalmykov937@gmail.com>
2019-01-29 21:27:27 +0300

    Выполнил задани номер 3 С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел. Добавил описание метода во втором задании

--- HomeWorkLesson2/Mission2/CalculateNumbers.cs
+++ HomeWorkLesson2/Mission2/CalculateNumbers.cs
@@ -4,8 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace Mission2
 {
+    /// <summary>
+    /// Подсчитывает колличество цифр в числе
+    /// </summary>
+
     class CalculateNumbers
     {
         public static int Calculate(string s)
---
+++ HomeWorkLesson2/Mission3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Калмыков
+//3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+
+namespace Mission3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number;
+            int summa;
+
+            summa = 0;
+
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+
+                if (number > 0 && number % 2 == 1)
+                {
+                    Console.WriteLine(number);
+                    summa += number;
+                }
+
+
+            } while (number !=0);
+
+            Console.WriteLine(summa);
+
+            Console.ReadKey();
+        }
+    }
+}
